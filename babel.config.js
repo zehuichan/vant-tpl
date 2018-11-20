994426c3efb2590754cdca220a3b7a094dfff286
jasconchan <jasonchenzehui@gmail.com>
2018-11-20 22:16:01 +0800

    ·

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    presets: [
-        '@vue/app'
-    ],
-    plugins: [
-        [
-            'import',
-            {libraryName: 'vant', libraryDirectory: 'es', style: true},
-            'vant'
-        ]
+  presets: [
+    '@vue/app'
+  ],
+  plugins: [
+    [
+      'import',
+      {libraryName: 'vant', libraryDirectory: 'es', style: true},
+      'vant'
     ]
+  ]
 }
