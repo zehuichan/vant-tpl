0c1a05b22cfe0f10a862633162eb20e9d1c5612b
jasonchan <jsoanchenzehui@gmail.com>
2019-04-17 15:47:25 +0800

    add vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,35 @@
+const port = 8080 // dev port
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development' ? 'error' : false,
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: false,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+  },
+
+  // proxy: process.env.VUE_APP_BASE_API,
+
+  // proxy: {
+  //   // change xxx-api/login => mock/login
+  //   // detail: https://cli.vuejs.org/config/#devserver-proxy
+  //   '/': {
+  //     target: process.env.VUE_APP_BASE_API,
+  //     secure: false,
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '/': '/'
+  //     }
+  //   }
+  // },
   css: {
-    loaderOptions: {
-      less: {
-        modifyVars: {
-          green: '#45bec7'
-        }
-      }
-    }
-  }
+    loaderOptions: {}
+  },
 }
